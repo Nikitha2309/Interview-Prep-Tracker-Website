097e2707cf974181e96081ecd941cf4758bc6fc9
SANNGESH P L <58561823+sanngesh23@users.noreply.github.com>
2021-02-20 21:18:48 +0530

    saving question

--- controllers/formController.js
+++ controllers/formController.js
@@ -1,10 +1,9 @@
 const Question = require("../models/Question");
 const app=require('../app');
 const {checkAdmin} = require('../middleware/authMiddleware');
-const Experience = require("../models/Experience");
 
 
-const handleErrorsQuestion=(err)=>{
+const handleErrors=(err)=>{
     console.log(err.message,err.code);
 
     //error messages
@@ -23,28 +22,6 @@ const handleErrorsQuestion=(err)=>{
 
     //invalid question 
     if (err.message.includes('Question validation failed')){
-        Object.values(err.errors).forEach(({properties})=>{
-          errors[properties.path]=properties.message;
-        });
-    }
-    return errors;
-
-}
-
-const handleErrorsExperience=(err)=>{
-    console.log(err.message,err.code);
-
-    //error messages
-     let errors={name:'',title:'',image:'',company:'',description:''};
-
-    //1.duplicate error code
-     if(err.code == 11000){
-        rrors.name= 'experience with this title already exists';
-        return errors;
-    }
-
-    //invalid question 
-    if (err.message.includes('Experience validation failed')){
         Object.values(err.errors).forEach(({properties})=>{
           errors[properties.path]=properties.message;
         });
@@ -95,33 +72,3 @@ module.exports.formQuestion_post = (req,res) => {
     }
 }
 
-module.exports.formExperience_get =(req,res) => {
-    res.render('formExperience',{ companys : app.companys});    
-}
-
-module.exports.formExperience_post = (req,res) => {
-   const {name,title,image,company,description}=req.body;
-   try
-   {
-       let valid;
-       if(checkAdmin)
-       {
-           valid=true;
-           console.log("admin added experience");
-       }
-       else
-       {
-           valid=false;
-           console.log("user added experience");
-       }
-       const question= Question.create({name,title,image,company,description,valid});
-       alert('some alert');
-       res.redirect('/some page');
-   }
-   catch(err)
-   {
-      const errors=handleErrorsExperience(err);
-      console.log(err);
-      res.status(400).json({errors});
-   }
-}
